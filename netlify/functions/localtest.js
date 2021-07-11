8a1c6065e2998d177cbe38cb4b4d867bdd76e347
Andrew Hatfield <andrew.hatfield@cynosureservices.com>
2021-07-11 15:18:50 +1000

    ??

--- netlify/functions/localtest.js
+++ netlify/functions/localtest.js
@@ -1,4 +1,4 @@
-const SEND_SMS = process.env.SEND_SMS
+// const SEND_SMS = process.env.SEND_SMS
 // const qs = require("qs")
 // const querystring = require("querystring")
 
@@ -7,16 +7,16 @@ const SEND_SMS = process.env.SEND_SMS
 // const twilioNumberFrom = process.env.TWILIO_SENDER_NUMBER
 
 // const client = require("twilio")(accountSID, accountToken)
-const MessagingResponse = require("twilio").twiml.MessagingResponse
+// const MessagingResponse = require("twilio").twiml.MessagingResponse
 
-const SGKEY = process.env.SENDGRID_API_KEY
-const SGToEmail = process.env.SG_TO_EMAIL
-const sgMail = require("@sendgrid/mail")
+// const SGKEY = process.env.SENDGRID_API_KEY
+// const SGToEmail = process.env.SG_TO_EMAIL
+// const sgMail = require("@sendgrid/mail")
 
-const HSKEY = process.env.HSKEY
-const HSContacts = "https://api.hubapi.com/crm/v3/objects/contacts/search"
+// const HSKEY = process.env.HSKEY
+// const HSContacts = "https://api.hubapi.com/crm/v3/objects/contacts/search"
 
-const fetch = require("node-fetch")
+// const fetch = require("node-fetch")
 
 exports.handler = async function (event, context, callback) {
   try {
@@ -30,62 +30,12 @@ exports.handler = async function (event, context, callback) {
     const originalSender = URI.From.replace("+61", "0")
     const originalBody = URI.Body.replace("+", " ")
 
-    //     const SGMsg = {
-    //       to: SGToEmail,
-    //       from: SGToEmail,
-    //       subject: "New SMS: " + originalSender,
-    //       text: "New SMS from: " + originalSender + "\n\n\nBody:\n\n" + originalBody,
-    //       html: "New SMS From: " + originalSender + "<br/><br/><br/>Body:<br/><br/>" + originalBody,
-    //     }
-
-    //     try {
-    //       sgMail.setApiKey(SGKEY)
-
-    //       sgMail
-    //         .send(SGMsg)
-    //         .then(() => {
-    //           console.log("Email Sent")
-    //         })
-    //         .catch((error) => {
-    //           console.error(error)
-    //         })
-    //     } catch (err) {
-    //       console.log("couldn't send email")
-    //     }
-
-    //     try {
-    //       const HSSearch = {
-    //         filterGroups: [
-    //           {
-    //             filters: [
-    //               {
-    //                 propertyName: "mobilephone",
-    //                 operator: "EQ",
-    //                 value: originalSender,
-    //               },
-    //             ],
-    //           },
-    //         ],
-    //       }
-
-    //       fetch(HSContacts + "?hapikey=" + HSKEY, {
-    //         method: "post",
-    //         headers: { "Content-Type": "application/json" },
-    //         body: JSON.stringify(HSSearch),
-    //       }).then(console.log(JSON.stringify(res)))
-    //     } catch (err) {
-    //       console.log("couldn't find HS contact")
-    //       console.log(err)
-    //     }*/
-
-    console.log("SEND_SMS=" + SEND_SMS)
+    // console.log("SEND_SMS=" + SEND_SMS)
     return callback(null, {
       statusCode: 200,
       contentType: "text/html",
       body:
-        "<html><head><title>a title</title></head><body>SEND_SMS=" +
-        SEND_SMS +
-        "<br/><br/>" +
+        "<html><head><title>a title</title></head><body>" +
         "originalSender: " +
         originalSender +
         "<br/><br/>" +
@@ -102,8 +52,57 @@ exports.handler = async function (event, context, callback) {
       body: "<html><title>title</title><body>" + console.log(err) + "</body></html>,",
     })
   }
+}
 
-  /*
+//     const SGMsg = {
+//       to: SGToEmail,
+//       from: SGToEmail,
+//       subject: "New SMS: " + originalSender,
+//       text: "New SMS from: " + originalSender + "\n\n\nBody:\n\n" + originalBody,
+//       html: "New SMS From: " + originalSender + "<br/><br/><br/>Body:<br/><br/>" + originalBody,
+//     }
+
+//     try {
+//       sgMail.setApiKey(SGKEY)
+
+//       sgMail
+//         .send(SGMsg)
+//         .then(() => {
+//           console.log("Email Sent")
+//         })
+//         .catch((error) => {
+//           console.error(error)
+//         })
+//     } catch (err) {
+//       console.log("couldn't send email")
+//     }
+
+//     try {
+//       const HSSearch = {
+//         filterGroups: [
+//           {
+//             filters: [
+//               {
+//                 propertyName: "mobilephone",
+//                 operator: "EQ",
+//                 value: originalSender,
+//               },
+//             ],
+//           },
+//         ],
+//       }
+
+//       fetch(HSContacts + "?hapikey=" + HSKEY, {
+//         method: "post",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(HSSearch),
+//       }).then(console.log(JSON.stringify(res)))
+//     } catch (err) {
+//       console.log("couldn't find HS contact")
+//       console.log(err)
+//     }*/
+
+/*
   try {
     const twiml = new MessagingResponse()
     await twiml.message(
@@ -117,27 +116,26 @@ exports.handler = async function (event, context, callback) {
 
 */
 
-  // try {
-  //   console.log("json stringify event.querystring")
-  //   console.log(JSON.stringify(event.queryStringParameters))
-  // } catch (err) {
-  //   console.log("couldn't console log JSON event querystring")
-  //   console.log("this is an error")
-  // }
+// try {
+//   console.log("json stringify event.querystring")
+//   console.log(JSON.stringify(event.queryStringParameters))
+// } catch (err) {
+//   console.log("couldn't console log JSON event querystring")
+//   console.log("this is an error")
+// }
 
-  // try {
-  //   console.log("event.querystring")
-  //   console.log(event.queryStringParameters)
-  // } catch (err) {
-  //   console.log("couldn't console log event querystring")
-  //   console.log("this is an error")
-  // }
+// try {
+//   console.log("event.querystring")
+//   console.log(event.queryStringParameters)
+// } catch (err) {
+//   console.log("couldn't console log event querystring")
+//   console.log("this is an error")
+// }
 
-  // try {
-  //   console.log("json stringify console")
-  //   console.log(JSON.stringify(context))
-  // } catch (err) {
-  //   console.log("couldn't console log JSON context")
-  //   console.log("this is an error")
-  // }
-}
+// try {
+//   console.log("json stringify console")
+//   console.log(JSON.stringify(context))
+// } catch (err) {
+//   console.log("couldn't console log JSON context")
+//   console.log("this is an error")
+// }
